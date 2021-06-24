9822ca6b5cf3343320f370b65965b8b4d817ddad
codewordjade <77465075+codewordjade@users.noreply.github.com>
2021-06-24 14:57:44 +0100

    added api key and url for real data

--- src/Weather.js
+++ src/Weather.js
@@ -1,84 +1,93 @@
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  let weatherData = {
-    city: "Leeds",
-    date: "Monday 08:00",
-    description: "Mostly Sunny",
-    temperature: 8,
-    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
-    humidity: 30,
-    wind: 10,
-  };
-  return (
-    <div className="container">
-      <div className="weather-app-wrapper">
-        <div className="weather-app">
-          <form className="mb-3">
-            <div className="row">
-              <div className="col-6">
-                <input
-                  type="search"
-                  placeholder="Type a city name"
-                  className="form-control"
-                  autoComplete="off"
-                />
-              </div>
-              <div className="col-6">
-                <input type="submit" value="search" className="btn btn-light" />
-                <button className="btn btn-light">current</button>
-              </div>
-            </div>
-          </form>
-          <h1>{weatherData.city}</h1>
-          <ul>
-            <li>
-              Last updated <span>{weatherData.date}</span>
-            </li>
-            <li>{weatherData.description}</li>
-          </ul>
-          <div className="row">
-            <div className="col-6">
-              <div className="d-flex weather-temperature">
-                <img
-                  src={weatherData.imgUrl}
-                  alt={weatherData.description}
-                  className="float-left"
-                />
-                <div className="float-left">
-                  <strong>{weatherData.temperature}</strong>
-                  <span className="units">
-                    <a href="/" className="active">
-                      °C
-                    </a>{" "}
-                    |<a href="/">°F</a>
-                  </span>
+  const [temperature, setTemperature] = useState(null);
+  const [ready, setReady] = useState(false);
+  function handleResponse(response) {
+    setTemperature(response.data.main.temp);
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="container">
+        <div className="weather-app-wrapper">
+          <div className="weather-app">
+            <form className="mb-3">
+              <div className="row">
+                <div className="col-6">
+                  <input
+                    type="search"
+                    placeholder="Type a city name"
+                    className="form-control"
+                    autoComplete="off"
+                  />
+                </div>
+                <div className="col-6">
+                  <input
+                    type="submit"
+                    value="search"
+                    className="btn btn-light"
+                  />
+                  <button className="btn btn-light">current</button>
                 </div>
               </div>
+            </form>
+            <h1>Leeds</h1>
+            <ul>
+              <li>
+                Last updated <span>Today</span>
+              </li>
+              <li>description</li>
+            </ul>
+            <div className="row">
+              <div className="col-6">
+                <div className="d-flex weather-temperature">
+                  <img src="#" alt="#" className="float-left" />
+                  <div className="float-left">
+                    <strong>{temperature}</strong>
+                    <span className="units">
+                      <a href="/" className="active">
+                        °C
+                      </a>{" "}
+                      |<a href="/">°F</a>
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="col-6">
+                <ul>
+                  <li>
+                    Humidity: <span>23</span> %
+                  </li>
+                  <li>
+                    Wind: <span>12</span> mph
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div className="col-6">
-              <ul>
-                <li>
-                  Humidity: <span>{weatherData.humidity}</span> %
-                </li>
-                <li>
-                  Wind: <span>{weatherData.wind}</span> mph
-                </li>
-              </ul>
-            </div>
+            <div className="weather-forecast"></div>
           </div>
-          <div className="weather-forecast"></div>
+          <small>
+            <a
+              href="https://github.com/codewordjade/Vanilla-weather-app"
+              target="blank"
+            >
+              Open-Source code
+            </a>
+            by Jade Knowles
+          </small>
         </div>
-        <small>
-          <a
-            href="https://github.com/codewordjade/Vanilla-weather-app"
-            target="blank"
-          >
-            Open-Source code
-          </a>
-          by Jade Knowles
-        </small>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "ebca13eec4bcc9a4c9201fa605468205";
+    let city = "Leeds";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
