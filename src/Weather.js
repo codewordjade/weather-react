310234c71495fa3ac12e3b271b751699b0a10e5b
codewordjade <77465075+codewordjade@users.noreply.github.com>
2021-07-12 10:22:59 +0100

    added search function

--- src/Weather.js
+++ src/Weather.js
@@ -3,10 +3,12 @@ import WeatherInfo from "./WeatherInfo.js";
 import axios from "axios";
 import "./Weather.css";
 export default function Weather(props) {
-  const [weatherData, setWeatherData] = useState(null);
-  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
   function handleResponse(response) {
     setWeatherData({
+      ready: true,
       temperature: response.data.main.temp,
       date: new Date(response.data.dt * 1000),
       wind: response.data.wind.speed,
@@ -15,14 +17,29 @@ export default function Weather(props) {
       description: response.data.weather[0].description,
       iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
     });
-    setReady(true);
   }
-  if (ready) {
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "ebca13eec4bcc9a4c9201fa605468205";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (weatherData.ready) {
     return (
       <div className="container">
         <div className="weather-app-wrapper">
           <div className="weather-app">
-            <form className="mb-3">
+            <form className="mb-3" onSubmit={handleSubmit}>
               <div className="row">
                 <div className="col-6">
                   <input
@@ -37,6 +54,7 @@ export default function Weather(props) {
                     type="submit"
                     value="search"
                     className="btn btn-light"
+                    onChange={handleCityChange}
                   />
                   <button className="btn btn-light">current</button>
                 </div>
@@ -48,9 +66,7 @@ export default function Weather(props) {
       </div>
     );
   } else {
-    const apiKey = "ebca13eec4bcc9a4c9201fa605468205";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+    search();
     return "Loading...";
   }
 }
