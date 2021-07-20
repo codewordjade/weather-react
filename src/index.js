872391f2ab5453dca456c17565434e75d24b3535
codewordjade <77465075+codewordjade@users.noreply.github.com>
2021-07-20 22:59:57 +0100

    installed bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import Weather from "./Weather";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
